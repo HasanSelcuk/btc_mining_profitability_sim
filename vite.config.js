b863ddbf68ee85e07dca2088761c5cadcf082d76
HasanSelcuk <91349753+HasanSelcuk@users.noreply.github.com>
2026-02-24 13:53:12 +0300

    Configure base path in Vite config
    Set base path for asset resolution to current directory.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwind from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwind()],
+  base: './', // Başına nokta koyarak "neredeysem oradan başla" diyoruz
 })
